5b1af439704acfcac0c92aaa251ae6022e08e831
Calvin May <calvinkylemay@gmail.com>
2021-02-06 15:00:57 -0500

    Converted displayContact() from Javascript to JQuery

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,190 +6,160 @@
 
 "use strict";
 
-((core) =>
-{
-    function displayHome()
-    {   
-        // Button Click events for all buttons on the page
-        //-Uses Jquery
-        $("button").on("mouseover", function()
-        {
-            console.log("Button Clicked!");
-        });
+((core) => {
+  function displayHome() {
 
-        // Button CLick event for the first button found on the page
-        //-Uses JavaScript
-        let myButton = document.querySelectorAll("button")[0];
+    let paragraphOneText =
+      "This is a simple site to demonstrate DOM Manipulation for ICE 1";
 
-        myButton.addEventListener("click", function()
-        {
-            console.log("Clicked button - js");
-        });
+    let paragraphOneElement = document.getElementById("paragraphOne");
 
-        console.log (myButton);
+    paragraphOneElement.textContent = paragraphOneText;
+    paragraphOneElement.className = "fs-5";
 
-        let paragraphOneText =
-          "This is a simple site to demonstrate DOM Manipulation for ICE 1";
+    // Step 1. document.createElement
+    let newParagraph = document.createElement("p");
+    // Step 2. configure the element
+    newParagraph.setAttribute("id", "paragraphTwo");
+    newParagraph.textContent = "...And this is paragraph two";
+    // Step 3. select the parent element
+    let mainContent = document.getElementsByTagName("main")[0];
+    // Step 4. Add / Insert the element
+    mainContent.appendChild(newParagraph);
 
-        let paragraphOneElement = document.getElementById("paragraphOne");
+    newParagraph.className = "fs-6";
 
-        paragraphOneElement.textContent = paragraphOneText;
-        paragraphOneElement.className = "fs-5";
+    // another way of injecting content
+    let paragraphDiv = document.createElement("div");
+    let paragraphThree = `<p id="paragraphThree" class="fs-7 fw-bold">And this is the Third Paragraph</p>`;
+    paragraphDiv.innerHTML = paragraphThree;
 
-        // Step 1. document.createElement
-        let newParagraph = document.createElement("p");
-        // Step 2. configure the element
-        newParagraph.setAttribute("id", "paragraphTwo");
-        newParagraph.textContent = "...And this is paragraph two";
-        // Step 3. select the parent element
-        let mainContent = document.getElementsByTagName("main")[0];
-        // Step 4. Add / Insert the element
-        mainContent.appendChild(newParagraph);
+    // insertions
 
-        newParagraph.className = "fs-6";
+    // example of inserting before a node
+    //newParagraph.before(paragraphDiv);
 
-        // another way of injecting content
-        let paragraphDiv = document.createElement("div");
-        let paragraphThree = `<p id="paragraphThree" class="fs-7 fw-bold">And this is the Third Paragraph</p>`;
-        paragraphDiv.innerHTML = paragraphThree;
+    // example of inserting after a node
+    newParagraph.after(paragraphDiv);
 
-        // insertions
+    // deletions
 
-        // example of inserting before a node
-        //newParagraph.before(paragraphDiv);
+    // example of removing a single element
+    //paragraphOneElement.remove();
 
-        // example of inserting after a node
-        newParagraph.after(paragraphDiv);
+    // example of removeChild
+    mainContent.removeChild(paragraphOneElement);
 
-        // deletions
+    // update / modification
+    //mainContent.firstElementChild.textContent = "Welcome Home!";
 
-        // example of removing a single element
-        //paragraphOneElement.remove();
-
-        // example of removeChild
-        mainContent.removeChild(paragraphOneElement);
-
-        // update / modification
-        //mainContent.firstElementChild.textContent = "Welcome Home!";
-
-        mainContent.innerHTML = `<h1 id="firstHeading">Welcome to WEBD6201 - Lab 1</h1>
+    mainContent.innerHTML = `<h1 id="firstHeading">Welcome to WEBD6201 - Lab 1</h1>
          <p id="paragraphOne" class="fs-3 fw-bold">This is my first Paragraph</p>
         `;
-        
-    }
-    
 
-    function displayAbout()
-    {
+  }
 
-    }
 
-    function displayProjects()
-    {
+  function displayAbout() {
 
-    }
+  }
 
-    function displayServices()
-    {
+  function displayProjects() {
 
-    }
+  }
 
-    function displayContact()
-    {
-        let messageArea = document.getElementById("messageArea");
-        messageArea.hidden = true;
+  function displayServices() {
 
-        // form validation
-        let fullName = document.getElementById("fullName");
-        fullName.addEventListener("blur", function() {
-            if(fullName.value.length < 2)
-            {
-                fullName.focus();
-                fullName.select();
-                messageArea.hidden = false;
-                messageArea.className = "alert alert-danger";
-                messageArea.textContent = "Please enter an appropriate Name";
-            }
-            else
-            {
-                messageArea.removeAttribute("class");
-                messageArea.hidden = true;
-            }
-        });
+  }
 
-        let sendButton = document.getElementById("sendButton");
-        sendButton.addEventListener("click", function(event){
-            //event.preventDefault();
-            
-            let contact = new core.Contact(fullName.value, contactNumber.value, emailAddress.value);
+    function displayContact() {
 
-            if(contact.serialize())
-            {
-              localStorage.setItem((localStorage.length + 1).toString(), contact.serialize());
-            }
-           
-        });
-    }
+      $("#messageArea").hide();
 
-    function displayContactList() 
-    {
-      if (localStorage.length > 0) 
+      // Form Validation using JQuery
+      $("#fullName").on("blur", () => 
       {
-        let contactList = document.getElementById("contactList");
-
-        let data = "";
-
-        for (let index = 0; index < localStorage.length; index++) 
+        if ($("#fullName").val().length < 2) 
         {
-          let contactData = localStorage.getItem((index + 1).toString());
+          $("#fullName").trigger("focus");
+          $("#fullName").trigger("select");
 
-          let contact = new core.Contact();
-          contact.deserialize(contactData);
+          $("#messageArea").addClass("alert alert-danger");
+          $("#messageArea").text("Please enter an appropriate Name");
+          $("#messageArea").show();
+        } 
+        else 
+        {
+          $("#messageArea").removeAttr("class");
+          $("#messageArea").hide();
+        }
+      });
 
-          data += `<tr>
+      $("#sendButton").on("click", () => 
+      {
+        
+        let contact = new core.Contact(fullName.value, contactNumber.value, emailAddress.value);
+
+        if (contact.serialize()) {
+          localStorage.setItem((localStorage.length + 1).toString(), contact.serialize());
+        }
+      })
+    }
+
+  function displayContactList() {
+    if (localStorage.length > 0) {
+      let contactList = document.getElementById("contactList");
+
+      let data = "";
+
+      for (let index = 0; index < localStorage.length; index++) {
+        let contactData = localStorage.getItem((index + 1).toString());
+
+        let contact = new core.Contact();
+        contact.deserialize(contactData);
+
+        data += `<tr>
           <th scope="row">${index + 1}</th>
           <td>${contact.FullName}</td>
           <td>${contact.ContactNumber}</td>
           <td>${contact.EmailAddress}</td>
         </tr>`;
-        }
-
-        contactList.innerHTML = data;
       }
+
+      contactList.innerHTML = data;
+    }
+  }
+
+
+
+  function Start() {
+    console.log("App Started...");
+
+    switch (document.title) {
+      case "Home":
+        displayHome();
+        break;
+      case "About":
+        displayAbout();
+        break;
+      case "Projects":
+        displayProjects();
+        break;
+      case "Services":
+        displayServices();
+        break;
+      case "Contact":
+        displayContact();
+        break;
+      case "Contact-List":
+        displayContactList();
+        break;
     }
 
-     
+  }
 
-    function Start()
-    {
-        console.log("App Started...");
+  window.addEventListener("load", Start);
 
-        switch (document.title) 
-        {
-          case "Home":
-              displayHome();
-            break;
-          case "About":
-              displayAbout();
-            break;
-          case "Projects":
-              displayProjects();
-            break;
-          case "Services":
-              displayServices();
-            break;
-          case "Contact":
-              displayContact();
-            break;
-          case "Contact-List":
-            displayContactList();
-          break;
-        }
-        
-    }
+  core.Start = Start;
 
-    window.addEventListener("load", Start);
-
-    core.Start = Start;
-
-}) (core || (core={}));
+})(core || (core = {}));
